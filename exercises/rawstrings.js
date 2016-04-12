cb6a3537ab3d4759df3516df213566c0117d8e75
Jamaleh <Jamaleh@hotmail.com>
2016-04-12 19:51:02 +1000

    complete rawstrings and fixed typos in spec

--- exercises/rawstrings.js
+++ exercises/rawstrings.js
@@ -22,6 +22,5 @@ the raw string provided
 */
 var bridgeString1 = function(myInputString) {
   
-  return 'I wish I could code  in javascript all day, every day!';
-
+  return myInputString + 'I wish I could code  in javascript all day, every day!';
 };
